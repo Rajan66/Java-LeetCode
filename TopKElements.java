5081fab07d46dc4ca272754f94de3571d3d3bca2
Rajan66 <rajanmaharjan042@gmail.com>
2024-10-17 23:25:58 +0545

    fix: correction on top-k elements in a list

--- TopKElements.java
+++ TopKElements.java
@@ -1,61 +1,70 @@
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class TopKElements {
     public static void main(String[] args) {
-//        int[] nums = {1, 1, 2, 2, 2, 3, 3, 3, 3};
-        int[] nums = {3, 0, 1, 0};
+        int[] testcase1 = {1, 1, 2, 2, 2, 3, 3, 3, 3};
+        int[] testcase2 = {3, 0, 1, 0};
+        int[] testcase3 = {1, 0, 1, 0, 2, 2};
 
-        int k = 1;
-        int[] result = topKFrequent(nums, k);
+        int k = 2;
 
-        System.out.println(Arrays.toString(result));
-        Arrays.stream(result).forEach(System.out::println);
+        System.out.println(Arrays.toString(topKFrequent(testcase1, k)));
+        System.out.println(Arrays.toString(topKFrequent(testcase2, 1)));
+        System.out.println(Arrays.toString(topKFrequent(testcase3, 2)));
+
+        System.out.println(Arrays.toString(topKFrequentBucketSort(testcase1, k)));
+        System.out.println(Arrays.toString(topKFrequentBucketSort(testcase2, 1)));
+        System.out.println(Arrays.toString(topKFrequentBucketSort(testcase3, 2)));
     }
 
     public static int[] topKFrequent(int[] nums, int k) {
         Map<Integer, Integer> count = new HashMap<>();
-        for (Integer i : nums) {
-            // we check the number and if it exists already we incresae the count
-            // if the number is new we add it to the hashmap then increase the count  0+1
-            if (!count.containsKey(i)) {
-                count.put(i, 0);
-            }
-            count.put(i, count.get(i) + 1);
+
+        for (int num : nums) {
+            count.put(num, count.getOrDefault(num, 0) + 1);
         }
-
-        // if the value is greater than say max = 0 it becomes a new max.
-        // if the value is not greater than max check with second max.
-        // if the value is not greater than second max ignore and continue with the list
-        int[] result = new int[k];
-
-        int max = 0;
-        int maxKey = 0;
-        int secondMax = 0;
-        int secondMaxKey = 0;
+        List<int[]> arr = new ArrayList<>();
         for (Map.Entry<Integer, Integer> map : count.entrySet()) {
-            if (map.getValue() > max) {
-                if (map.getValue() > secondMax) {
-                    secondMaxKey = maxKey;
-                    secondMax = max;
+            arr.add(new int[]{map.getValue(), map.getKey()});
+        }
+
+        arr.sort((a, b) -> b[0] - a[0]);
+
+        int[] result = new int[k];
+        for (int i = 0; i < k; i++) {
+            result[i] = arr.get(i)[1];
+        }
+        return result;
+    }
+
+    public static int[] topKFrequentBucketSort(int[] nums, int k) {
+        Map<Integer, Integer> count = new HashMap<>();
+        List<Integer>[] freq = new List[nums.length + 1];
+        // this will create an array of lists.
+        // Array freq will hold multiple array lists inside it.
+
+        for (int i = 0; i < freq.length; i++) {
+            freq[i] = new ArrayList<>();
+        }
+
+        for (int num : nums) {
+            count.put(num, count.getOrDefault(num, 0) + 1);
+        }
+
+        for (Map.Entry<Integer, Integer> entry : count.entrySet()) {
+            freq[entry.getValue()].add(entry.getKey());
+        }
+
+        int[] result = new int[k];
+        int index = 0;
+        for (int i = freq.length - 1; i > 0 && index < k; i--) {
+            for (int item : freq[i]) {
+                result[index++] = item;
+                if (index == k) {
+                    return result;
                 }
-                maxKey = map.getKey();
-                max = map.getValue();
-            } else if (map.getValue() > secondMax) {
-                secondMax = map.getValue();
-                secondMaxKey = map.getKey();
             }
         }
-
-        result[0] = maxKey;
-        if (k > 1) {
-            result[1] = secondMaxKey;
-        }
-
-//        for (Map.Entry<Integer, Integer> map : count.entrySet()) {
-//            System.out.println(map.getKey() + " : " + map.getValue());
-//        }
 
         return result;
     }
