574b750ad3f7fbbcbdb46e08321c974e41de5939
Rajan66 <rajanmaharjan042@gmail.com>
2024-11-10 22:42:20 +0545

    added another solution for valid anagram

--- ValidAnagram.java
+++ ValidAnagram.java
@@ -41,4 +41,22 @@ public class ValidAnagram {
         }
         return true;
     }
+
+    // runtime is slower tho idk, memory is better due to only one map.
+    public boolean isAnagramBetter(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        Map<Character, Integer> count = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            count.put(s.charAt(i), count.getOrDefault(s.charAt(i), 0) + 1);
+            count.put(t.charAt(i), count.getOrDefault(t.charAt(i), 0) - 1);
+        }
+        for(int val: count.values()){
+            if(val != 0){
+                return false;
+            }
+        }
+        return true;
+    }
 }
