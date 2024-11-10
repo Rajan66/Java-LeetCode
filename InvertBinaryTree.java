f7f94086df701718a112406242b95248f87bcfd9
Rajan66 <rajanmaharjan042@gmail.com>
2024-11-10 21:53:14 +0545

    Easy tree problems: fun

--- InvertBinaryTree.java
+++ InvertBinaryTree.java
@@ -19,40 +19,17 @@ class TreeNode {
 
 public class InvertBinaryTree {
     public TreeNode invertTree(TreeNode root) {
-        TreeNode tempNode = root;
         // we have root node which has left and right
         // say root node is 1 and left and right are 2 and 3 respectively.
         if (root == null) {
-            return root;
+            return null;
         }
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
 
-        tempNode.left = root.right;
-        tempNode.right = root.left;
-
-        swapLeft(tempNode.left);
-        swapRight(tempNode.right);
-
-        return tempNode;
-    }
-
-    public TreeNode swapLeft(TreeNode leftRoot) {
-        TreeNode node = leftRoot;
-        if (node.left == null || node.right == null) {
-            return node;
-        }
-        node.left = leftRoot.right;
-        node.right = leftRoot.left;
-        return swapLeft(node.left);
-    }
-
-    public TreeNode swapRight(TreeNode leftRoot) {
-        TreeNode node = new TreeNode();
-
-        if (node.left == null || node.right == null) {
-            return node;
-        }
-        node.left = leftRoot.right;
-        node.right = leftRoot.left;
-        return swapRight(node.right);
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
     }
 }
