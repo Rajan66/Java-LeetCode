e4635e0822d825a3442cc977ef6243c350c60166
Rajan66 <rajanmaharjan042@gmail.com>
2024-11-12 22:35:50 +0545

    valid palindrome revision

--- Solution.java
+++ Solution.java
@@ -62,5 +62,30 @@ public class Solution {
 //        return Math.max(leftDepth, rightDepth) + 1;
     }
 
+    public boolean isPalindrome(String s) {
+        int i = 0;
+        int j = s.length() - 1;
+
+        while (i < j) {
+            Character start = s.charAt(i);
+            Character end = s.charAt(j);
+
+            if (!Character.isLetterOrDigit(start)) {
+                i++;
+                continue;
+            }
+            if (!Character.isLetterOrDigit(end)) {
+                j--;
+                continue;
+            }
+            if (Character.toLowerCase(start) != Character.toLowerCase(end)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
 }
 
