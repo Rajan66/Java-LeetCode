3d45c76625910a332b0d34635863f610a58fdbad
Rajan66 <rajanmaharjan042@gmail.com>
2024-11-14 23:11:42 +0545

    fibonacci

--- InterviewTest/FibonacciSequence.java
+++ InterviewTest/FibonacciSequence.java
@@ -4,11 +4,12 @@ public class FibonacciSequence {
     public static void main(String[] args) {
         printFibonacciSequence(5);
     }
+
     public static void printFibonacciSequence(int n) {
         int num1 = 0;
         int num2 = 1;
 
-        for(int i=0;i<n;i++){
+        for (int i = 0; i < n; i++) {
             System.out.print(num1 + " ");
             int next = num1 + num2;
             num1 = num2;
