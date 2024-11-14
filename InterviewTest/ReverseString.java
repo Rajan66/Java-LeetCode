f7e5f7c6062804d43e4053e85afa7b5502c8904a
Rajan66 <rajanmaharjan042@gmail.com>
2024-11-14 23:12:18 +0545

    two sum and reverse string problem

--- InterviewTest/ReverseString.java
+++ InterviewTest/ReverseString.java
@@ -1,2 +1,18 @@
-package InterviewTest;public class ReverseString {
+package InterviewTest;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        reverseString("Manny");
+        reverseString("69abc123");
+
+    }
+
+    public static void reverseString(String s) {
+        StringBuilder result = new StringBuilder();
+        for (int i = s.length() - 1; i >= 0; i--) {
+            result.append(s.charAt(i));
+        }
+        System.out.println("Original String: " + s);
+        System.out.println("Reversed String: " + result);
+    }
 }
--- TwoSum.java
+++ TwoSum.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class TwoSum {
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class TwoSum {
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 5};
+        System.out.println(Arrays.toString(twoSum(nums, 9)));
+
+    }
+
+    public int[] twoSumBruteForce(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[0];
+    }
+
+
+    public static int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            int diff = target - num;
+
+            if (map.containsKey(diff)) {
+                return new int[]{i, map.get(diff)};
+            }
+            map.put(num, i);
+        }
+        return new int[0];
+    }
 }
