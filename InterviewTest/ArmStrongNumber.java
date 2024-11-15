58f2c0245a0ac186359b09313acbe4192133ddb9
Rajan66 <rajanmaharjan042@gmail.com>
2024-11-15 18:17:23 +0545

    Armstrong number

--- InterviewTest/ArmStrongNumber.java
+++ InterviewTest/ArmStrongNumber.java
@@ -1,4 +1,22 @@
 package InterviewTest;
 
 public class ArmStrongNumber {
+    public static void main(String[] args) {
+        System.out.println(findArmStrongNum(153));
+        System.out.println(findArmStrongNum(253));
+        System.out.println(findArmStrongNum(1));
+        System.out.println(findArmStrongNum(53));
+        System.out.println(findArmStrongNum(2883));
+    }
+
+    public static boolean findArmStrongNum(int n) {
+        int temp = n;
+        int result = 0;
+        while (temp > 0) {
+            int digit = temp % 10;
+            result = (int) Math.pow(digit, 3) + result;
+            temp = temp / 10;
+        }
+        return result == n;
+    }
 }
