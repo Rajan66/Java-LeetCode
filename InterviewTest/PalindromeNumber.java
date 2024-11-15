d425e2e2c69aa50ef24274e6b44c2e77b7999dff
Rajan66 <rajanmaharjan042@gmail.com>
2024-11-15 18:25:46 +0545

    Valid Palindrome String revise

--- InterviewTest/PalindromeNumber.java
+++ InterviewTest/PalindromeNumber.java
@@ -6,6 +6,12 @@ public class PalindromeNumber {
         System.out.println(isPalindrome(121));
         System.out.println(isPalindrome(1113));
         System.out.println(isPalindrome(111));
+
+        System.out.println(isPalindromeString("Hello"));
+        System.out.println(isPalindromeString("A man, a plan, a canal, Panama!"));
+        System.out.println(isPalindromeString("No 'x' in Nixon"));
+        System.out.println(isPalindromeString("Madam, in Eden, I'm Adam."));
+        System.out.println(isPalindromeString("Was it a car or a cat I saw?"));
     }
 
     public static boolean isPalindrome(int n) {
@@ -18,5 +24,30 @@ public class PalindromeNumber {
             temp = temp / 10;
         }
         return n == result;
+    }
+
+    public static boolean isPalindromeString(String s) {
+        int i = 0;
+        int j = s.length() - 1;
+
+        while (i < j) {
+            char start = s.charAt(i);
+            char end = s.charAt(j);
+
+            if (!Character.isLetterOrDigit(start)) {
+                i++;
+                continue;
+            }
+            if (!Character.isLetterOrDigit(end)) {
+                j--;
+                continue;
+            }
+            if (Character.toLowerCase(start) != Character.toLowerCase(end)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
     }
 }
