f26cbad581927b8bc36294895d91e72f896b93bf
Rajan66 <rajanmaharjan042@gmail.com>
2024-11-16 07:05:17 +0545

    two sum revision

---
+++ InterviewTest/TwoSum.java
@@ -0,0 +1,20 @@
+package InterviewTest;
+
+import java.util.HashMap;
+
+public class TwoSum {
+    public static int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            int diff = target - num;
+
+            if (map.containsKey(diff)) {
+                return new int[]{i, map.get(diff)};
+            }
+            map.put(num, i);
+        }
+        return new int[0];
+    }
+
+}
