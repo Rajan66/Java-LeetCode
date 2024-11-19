6100ebcd38e269ba9fc71e52b81ca5fbf056aaa4
Rajan66 <rajanmaharjan042@gmail.com>
2024-11-19 22:33:39 +0545

    Top K elements medium revise

--- TopKElements.java
+++ TopKElements.java
@@ -68,4 +68,34 @@ public class TopKElements {
 
         return result;
     }
+
+
+    public static int[] topKFrequentBucketSort1(int[] nums, int k) {
+        HashMap<Integer, Integer> count = new HashMap<>();
+        List<Integer>[] freq = new List[nums.length + 1];
+
+        for (int i = 0; i < nums.length; i++) {
+            freq[i] = new ArrayList<>();
+        }
+
+        for (int num : nums) {
+            count.put(num, count.getOrDefault(num, 0) + 1);
+        }
+
+        for (Map.Entry<Integer, Integer> entry : count.entrySet()) {
+            freq[entry.getValue()].add(entry.getKey());
+        }
+
+        int[] result = new int[k];
+        int index = 0;
+        for (int i = freq.length - 1; i > 0 && index < k; i--) {
+            for (int item : freq[i]) {
+                result[++index] = item;
+                if (index == k) {
+                    return result;
+                }
+            }
+        }
+        return result;
+    }
 }
