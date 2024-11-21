22193d9e196729a852696f9fad479dd59a3922a9
Rajan66 <rajanmaharjan042@gmail.com>
2024-11-21 23:28:21 +0545

    max depth of tree revise

--- MaxDepthTree.java
+++ MaxDepthTree.java
@@ -13,11 +13,11 @@ public class MaxDepthTree {
     }
 
     public int maxDepth1(TreeNode root) {
-        if (root == null) return 0;
-
+        if (root == null) {
+            return 0;
+        }
         int leftDepth = maxDepth(root.left);
         int rightDepth = maxDepth(root.right);
-
         return Math.max(leftDepth, rightDepth) + 1;
     }
 }
