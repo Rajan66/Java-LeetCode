2be76954dc0e2e98c5e2d5cbc1a9f7b891fa0579
Rajan66 <rajanmaharjan042@gmail.com>
2024-11-22 23:28:02 +0545

    bsearch

--- BSearch.java
+++ BSearch.java
@@ -24,4 +24,25 @@ public class BSearch {
 
         return -1;
     }
+
+
+    public int binarySearchQuick(int[] nums, int target){
+        int low = 0;
+        int high = nums.length-1;
+
+        while(low<high){
+            int mid = (low +high)/2;
+
+            if(nums[mid] == target){
+                return mid;
+            }
+            else if(nums[mid] > target){
+                high = mid - 1;
+            }
+            else if(nums[mid] < target){
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
 }
