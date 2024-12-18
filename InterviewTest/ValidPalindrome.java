d0fdd62fd5bb7221fcd17f2b9483b40049ae08d3
Rajan66 <rajanmaharjan042@gmail.com>
2024-12-18 17:34:38 +0545

    valid palindrome revise

--- InterviewTest/ValidPalindrome.java
+++ InterviewTest/ValidPalindrome.java
@@ -25,4 +25,33 @@ public class ValidPalindrome {
         }
         return true;
     }
+
+    public boolean isPalindromeStr(String input) {
+        int i = 0;
+        int j = input.length() - 1;
+
+        while (i < j) {
+            char s = input.charAt(i);
+            char t = input.charAt(j);
+
+            if (!Character.isLetterOrDigit(s)) {
+                i++;
+                continue;
+            }
+
+            if (!Character.isLetterOrDigit(t)) {
+                j--;
+                continue;
+            }
+
+            if (Character.toLowerCase(s) != Character.toLowerCase(t)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
 }
+
+
